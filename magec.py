e4194bc198adecf6e5f9b18606cb5641971dba0e
Chris Kuehl <chris@techxonline.net>
2013-04-05 21:29:46 -0400

    Detecting MySQL settings.

--- magec.py
+++ magec.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # clones a remote Magento installation into the current working directory
 import sys, os, time
+import xml.etree.ElementTree as etree 
 
 if len(sys.argv) != 2:
 	print("usage: magec remote_path")
@@ -15,14 +16,34 @@ if not remote.endswith("/"):
 	remote += "/"
 
 remote_user_host = remote[:remote.find(":")]
+remote_host = remote_user_host[(remote.find("@") + 1):]
 remote_path = remote[(remote.find(":") + 1):]
-print(remote_path)
 
 print("Cloning from Magento repository on {} at {}...".format(remote_user_host, remote_path))
 
 # clone the remote directory into the current directory
 start = time.time()
-os.system('ssh -C {} "cd {} && tar cv - --exclude=var *" | tar xvf -'.format(remote_user_host, remote_path))
+#os.system('ssh -C {} "cd {} && tar cv - --exclude=var *" | tar xvf -'.format(remote_user_host, remote_path))
 end = time.time()
 
 print("Files cloned in {}s.".format(int(end - start)))
+
+# detect MySQL information
+conf = etree.parse("app/etc/local.xml")
+root = conf.getroot()
+
+nhost = root.find("global/resources/default_setup/connection/host")
+nusername = root.find("global/resources/default_setup/connection/username")
+npassword = root.find("global/resources/default_setup/connection/password")
+ndbname = root.find("global/resources/default_setup/connection/dbname")
+
+thost = nhost.text
+tusername = nusername.text
+tpassword = npassword.text
+tdbname = ndbname.text
+
+print("Detected MySQL settings:")
+print("\tHost: {}".format(thost))
+print("\tUsername: {}".format(tusername))
+print("\tPassword: {}".format(tpassword))
+print("\tDB Name: {}".format(tdbname))
